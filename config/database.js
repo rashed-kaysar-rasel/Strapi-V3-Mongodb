a4d5c970bbb5ab6375244ef940c281d603cada0b
Rashed Kaysar <rashedkaysar321@gmail.com>
2025-10-16 18:19:46 +0600

    change db to default

--- config/database.js
+++ config/database.js
@@ -1,17 +1,9 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        client: 'mongo',
-        uri: env('DATABASE_URI', 'mongodb://localhost:27017/Blog'),
-        database: env('DATABASE_NAME', 'Blog'),
-        srv: false,
-      },
-      options: {
-        ssl: false,
-      },
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
     },
+    useNullAsDefault: true,
   },
 });
